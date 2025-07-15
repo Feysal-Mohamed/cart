110a995aeb6dd39fe5b84a8defd130249fc9d6c2
feysalow <fyslcs567@gmail.com>
2025-07-15 21:15:57 +0300

    update

--- main.js
+++ main.js
@@ -6,21 +6,25 @@ const span1=document.querySelector("#span1")
 
 let x=0;
 let y=600.72;
+let d='$'
+
 
 btn1.addEventListener("click",()=>{
     x++;
     text.innerHTML=x
-    span1.innerHTML=y*x
+    span1.innerHTML=d+y*x
 })
 
 btn2.addEventListener("click",()=>{
     if(x>1){
     x--;
     text.innerHTML=x
-    span1.innerHTML=y*x
+    span1.innerHTML=d+y*x
     }
     else{
     text.innerHTML=1
+    span1.innerHTML=d+y*x
+
 
     }
 })
